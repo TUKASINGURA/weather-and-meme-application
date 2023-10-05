b9c749433371fadf02f56ee00195e0d73ae0f4aa
tukasingura <tukasingurarichard551@gmail.com>
2023-10-05 16:22:08 +0300

    weather application combined with the memeApllication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,27 +40,36 @@ android {
 
 dependencies {
 
+    constraints {
+        implementation("jakarta.activation:jakarta.activation-api:1.2.1") {
+            because("kotlin-stdlib-jdk7 is now a part of kotlin-stdlib")
+        }
+        implementation("com.sun.activation:javax.activation:1.2.0") {
+            because("kotlin-stdlib-jdk8 is now a part of kotlin-stdlib")
+        }
+    }
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
 
-    //classpath'com.android.tools.build:gradle:3.2.1'
+   // implementation("com.android.tools.build:gradle:8.1.1")
 // implementation
 
     //retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
 //Picasso
-    implementation ("com.squareup.picasso:picasso:2.8")
-//
-////Coroutines
-//
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core :1.6.0")
+    implementation ("com.squareup.picasso:picasso:2.71828")
+
+//Coroutines
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 }
--- app/src/main/java/com/example/weatherappexample/MainActivity.kt
+++ app/src/main/java/com/example/weatherappexample/MainActivity.kt
@@ -4,9 +4,12 @@ package com.example.weatherappexample
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.example.weatherappexample.Utils.RetrofitInstance
 import com.example.weatherappexample.databinding.ActivityMainBinding
 import com.squareup.picasso.Picasso
@@ -20,101 +23,125 @@ import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val floatingButton= binding.fab
-            floatingButton.setOnClickListener{
-            startActivity(Intent(this,MemesAPP::class.java))
+        val floatingButton = binding.fab
+        floatingButton.setOnClickListener {
+            startActivity(Intent(this, MemesAPP::class.java))
         }
 
-
-
-        getCurrentWeather()
-    }
-
-    private fun getCurrentWeather() {
-        GlobalScope.launch(Dispatchers.IO) {
-            val response = RetrofitInstance().createRetrofit().getCurrentWeather(
-                "Kampala",
-                "metric",
-                applicationContext.getString(R.string.api_key)
-            )
-            withContext(Dispatchers.Main) {
-
-                val textDisplayOnScreen = findViewById<TextView>(R.id.tv_temp)
-                textDisplayOnScreen.text = "${response.main.temp.toInt()}°C"
-
-                val textDisplayOnScre = findViewById<TextView>(R.id.tv_location)
-                textDisplayOnScre.text = "City ${response.name}"
-
-                val timeUpdate = findViewById<TextView>(R.id.tv_update_time)
-                timeUpdate.text = "TimeZone ${response.timezone}"
-
-                val weatherDescription = findViewById<TextView>(R.id.tv_status)
-
-
-                val minTemperature = findViewById<TextView>(R.id.tv_min_temp)
-                minTemperature.text = "Min ${response.main.temp_min}°C"
-
-                val maxTemperature = findViewById<TextView>(R.id.tv_max_temp)
-                maxTemperature.text = "Max ${response.main.temp_max}°C"
-
-                val sunset = findViewById<TextView>(R.id.tvSunset)
-                sunset.text =dateFormatConverter(
-                    response.sys.sunset.toLong()
-                )
-                val sunrise = findViewById<TextView>(R.id.tvSunrise)
-                sunrise.text = dateFormatConverter(
-                    response.sys.sunrise.toLong()
-                )
-
-                val windSpeed = findViewById<TextView>(R.id.tvWind)
-                windSpeed.text = "${response.wind.speed}km/h"
-
-                val pressure = findViewById<TextView>(R.id.tvPressure)
-                pressure.text = "${response.main.pressure}Pa"
-
-                val humidity = findViewById<TextView>(R.id.tvHumidity)
-                humidity.text = "${response.main.humidity}%"
-
-                //this Block of code is for changing the icon on the weather application UI FROM the weather API
-                val iconId = response.weather[0].icon
-                val imageWeather = findViewById<ImageView>(R.id.img_weather)
-                val imgUrl = "https://openweathermap.org/img/wn/$iconId@4x.png"
-
-                Picasso.get().load(imgUrl).into(imageWeather)
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // Handle query submission (e.g., when the user presses Enter)
+                getCurrentWeather(query)
+                return true
             }
-/**
-         =  try {
 
-         }
-             RetrofitInstance().createRetrofit().getCurrentWeather("kampala","metric",applicationContext.getString(R.string.api_key))
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // Handle query text changes (e.g., live search)
+                // You can use newText for incremental search updates
+                return true
+            }
+        })
+        getCurrentWeather("Kampala")//Kampala as the default city
+    }
 
-           }catch (e: IOException){
-               Toast.makeText(applicationContext, "app Error ${e.message}" , Toast.LENGTH_LONG)
-                   .show()
-             return@launch
-           } catch (e:HttpException){
-               Toast.makeText(applicationContext,"http error ${e.message}" , Toast.LENGTH_LONG)
-                   .show()
-             return@launch
-           }
-           if (response.isSuccessful&& response.body()!=null){
-               withContext(Dispatchers.Main){
+    private fun getCurrentWeather(city: String?) {
+        binding.loading.visibility = View.VISIBLE
+        try {
+            GlobalScope.launch(Dispatchers.IO) {
+                val response = RetrofitInstance().createRetrofit().getCurrentWeather(
+                    city!!,
+                    "metric",
+                    applicationContext.getString(R.string.api_key)
+                )
+                withContext(Dispatchers.Main) {
 
-                   val textDisplayOnScreen=findViewById<TextView>(R.id.tv_temp)
-                  textDisplayOnScreen.text = "Pressure: ${response.body()!!.main.pressure}"
-               }
-           }
-*/
+                    val textDisplayOnScreen = findViewById<TextView>(R.id.tv_temp)
+                    textDisplayOnScreen.text = "${response.main.temp.toInt()}°C"
+
+                    val textDisplayOnScre = findViewById<TextView>(R.id.tv_location)
+                    textDisplayOnScre.text = "City ${response.name}"
+
+                    val timeUpdate = findViewById<TextView>(R.id.tv_update_time)
+                    timeUpdate.text = "TimeZone ${response.timezone}"
+
+                    val minTemperature = findViewById<TextView>(R.id.tv_min_temp)
+                    minTemperature.text = "Min ${response.main.temp_min}°C"
+
+                    val maxTemperature = findViewById<TextView>(R.id.tv_max_temp)
+                    maxTemperature.text = "Max ${response.main.temp_max}°C"
+
+                    val sunset = findViewById<TextView>(R.id.tvSunset)
+                    sunset.text = dateFormatConverter(
+                        response.sys.sunset.toLong()
+                    )
+                    val sunrise = findViewById<TextView>(R.id.tvSunrise)
+                    sunrise.text = dateFormatConverter(
+                        response.sys.sunrise.toLong()
+                    )
+
+                    val windSpeed = findViewById<TextView>(R.id.tvWind)
+                    windSpeed.text = "${response.wind.speed}km/h"
+
+                    val pressure = findViewById<TextView>(R.id.tvPressure)
+                    pressure.text = "${response.main.pressure}Pa"
+
+                    val humidity = findViewById<TextView>(R.id.tvHumidity)
+                    humidity.text = "${response.main.humidity}%"
+
+                    //this Block of code is for changing the icon on the weather application UI FROM the weather API
+                    val iconId = response.weather[0].icon
+                    val imageWeather = findViewById<ImageView>(R.id.img_weather)
+                    val imgUrl = "https://openweathermap.org/img/wn/$iconId@4x.png"
+
+                    Picasso.get().load(imgUrl).into(imageWeather)
+                    binding.tvStatus.text = response.weather[0].main
+                    binding.tvFeelsLike.text = response.weather[0].description
+
+                    binding.loading.visibility = View.GONE
+                }
+                /**
+                =  try {
+
+                }
+                RetrofitInstance().createRetrofit().getCurrentWeather("kampala","metric",applicationContext.getString(R.string.api_key))
+
+                }catch (e: IOException){
+            1    Toast.makeText(applicationContext, "app Error ${e.message}" , Toast.LENGTH_LONG)
+                .show()
+                return@launch
+                } catch (e:HttpException){
+                Toast.makeText(applicationContext,"http error ${e.message}" , Toast.LENGTH_LONG)
+                .show()
+                return@launch
+                }
+                if (response.isSuccessful&& response.body()!=null){
+                withContext(Dispatchers.Main){
+
+                val textDisplayOnScreen=findViewById<TextView>(R.id.tv_temp)
+                textDisplayOnScreen.text = "Pressure: ${response.body()!!.main.pressure}"
+                }
+                }
+                 */
+            }
+        }catch (e: Exception){
+            e.printStackTrace()
+            makeToast("No Internet Connection!!")
         }
     }
-/**THE foolowing function is for changing the time
- *  from milliseconds to hours to be displayed on the UI*/
+
+    fun makeToast(text: String){
+        Toast.makeText(applicationContext,text,Toast.LENGTH_LONG).show()
+    }
+
+    /**THE foolowing function is for changing the time
+     *  from milliseconds to hours to be displayed on the UI*/
     private fun dateFormatConverter(date: Long): String {
 
         return SimpleDateFormat(
